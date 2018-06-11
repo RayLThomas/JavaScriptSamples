6776d85dc782ac935265f78480df7c3b0f39818c
Ray Thomas II <raythomasclient@gmail.com>
2018-06-11 06:25:00 -0400

    added solutions
    added solutions

--- scripts/most-digits.js
+++ scripts/most-digits.js
@@ -6,11 +6,11 @@ If two numbers in the argument array have the same number of digits, return the 
 */
 
 function findLongest(array){
-  for (var largest = 0, i = 0; i < array.length; i++){
-    array[i] > largest ? largest = array[i] : false;
+  for (largest = 0, i = 0; i < array.length; i++){
+    array[i].toString().length > largest.toString().length ? largest = array[i] : false;
   }
   return largest;
 }
 
 
-document.getElementById("output").innerHTML = findLongest([12, 199, 1209, 3 , 15]);
+document.getElementById("output").innerHTML = findLongest([12, 199, 1209, 9999, 3 , 15]);
